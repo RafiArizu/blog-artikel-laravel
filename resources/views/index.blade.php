d12dba75ca1486a70818e9cfea2c3d6dbf1a68d5
RafiArizu <rafiariz95@Gmail.com>
2025-10-01 21:06:29 +0700

    First commit - upload project framework

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+  <title>Belajar TailwindCSS</title>
+</head>
+<body>
+
+  <section>
+    <h2>Spacing</h2>
+    <div class="mr-8">Margin</div>
+  </section>
+
+</body>
+</html>
